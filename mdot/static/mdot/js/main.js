b4a334b8ccdd1f2a7134b3a6a7c561cacffad4ae
Charlon Palacay <charlon@gmail.com>
2015-07-17 12:08:49 -0700

    Javascript to handle featured slide callbacks.

--- mdot/static/mdot/js/main.js
+++ mdot/static/mdot/js/main.js
@@ -1,14 +1,24 @@
 // document.ready shorthand
 $(function() {
     
+    // initiate the slide
  	$( '#feature_slider' ).sliderPro({
      	width : '100%',
      	autoHeight : true,
      	buttons : false,
      	autoplay : false,
      	
-     	thumbnailHeight : 30,
+     	thumbnailHeight : 'auto',
      	thumbnailWidth : 70,
+     	
     });
  	
-}); 
+ 	// handle what happens when a slide is shown
+ 	$( '#feature_slider' ).on( 'gotoSlideComplete', function( event ) {
+        console.log("you are on slide: " + event.index);
+    })
+
+
+});
+
+ 
