2b31c0caed7c4fe9e25abeaa56ba921a5eafbbec
Charlon Palacay <charlon@gmail.com>
2015-07-21 10:26:28 -0700

    Fixes js logic for handling sticky navigation.

--- mdot/static/mdot/js/main.js
+++ mdot/static/mdot/js/main.js
@@ -19,21 +19,36 @@ $(function() {
         console.log("you are on slide: " + event.index);
     })
     
-    var static_header = $(".sp-thumbnails-container");
-    var stickyRibbonTop = $('.mdot-resources').offset().top + 270;
-          
+    
     $(window).scroll(function(){
-        if( $(window).scrollTop() > stickyRibbonTop ) {
-            static_header.addClass("stuuuuuuuuuuck");
-            $("#thumb_back").addClass("stuuuuuuuuuuck");
+        
+        var nav_container = $(".sp-thumbnails-container");
+        var nav_header = $(".mdot-navbar").height();
+        var user_header = $(".mdot-user").height();    
+        var feaure_header = $(".mdot-feature").height() - 40;
+        var sticky_point = nav_header + user_header + feaure_header;
+        var page_top = $(window).scrollTop() + nav_header;
+        
+        /*
+        console.log("scrolltop: " + page_top);
+        console.log("resourcetop: " + stuck_location);
+        console.log("navheight: " + nav_header);
+        console.log("userheight: " + user_header);
+        console.log("fetheight: " + feaure_header);
+        */
+                
+        if( page_top > sticky_point) {
+            nav_container.addClass("stuck");
+            $("#nav_back").addClass("stuck");
+            $("#nav_shim").show();
         }
         else {
-            static_header.removeClass("stuuuuuuuuuuck");
-            $("#thumb_back").removeClass("stuuuuuuuuuuck");
+            nav_container.removeClass("stuck");
+            $("#nav_back").removeClass("stuck");
+            $("#nav_shim").hide();
         }
     });
 
 
-    
 
 });
