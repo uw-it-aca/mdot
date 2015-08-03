daf29e49be4284eec7bc5c1c9c807d70183870bc
haughar <haughar@uw.edu>
2015-08-03 15:55:32 -0700

    Change the json returned into a ClientResource for use by the template.

--- mdot/views.py
+++ mdot/views.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from mdot_rest_client.client import MDOT
 import urllib
 import json
 
 
 def home(request):
-    return render_to_response('mdot/home.html',
+    params = {'resources': MDOT().get_resources()}
+    return render_to_response('mdot/home.html', params,
                               context_instance=RequestContext(request)
                               )
