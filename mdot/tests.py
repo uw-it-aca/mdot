ce503d31fe46c8ed95122a6a72e8318d6ff20886
haughar <haughar@uw.edu>
2015-08-03 18:52:29 -0700

    Add methods to client resource class to check for types of links.

--- mdot/tests.py
+++ mdot/tests.py
@@ -166,5 +166,155 @@ class MdotClientTest(TestCase):
         with self.assertRaises(TypeError):
             MDOT()._python_list_to_resources_model_list(fake_list)
 
+    # Make sure that has_ios returns true if the resource has an iOS link
+    def test_if_has_ios_true(self):
+        fake_list = [{u'accessible': False,
+                      u'feature_desc': u'This is a test',
+                      u'title': u'ITConnect',
+                      u'image': u'http://localhost:8000/media/\
+                      uploads/screenshot_CprR5Dk.jpg',
+                      u'created_date': u'2015-07-31T19:18:43.771637Z',
+                      u'campus_seattle': True,
+                      u'campus_bothell': False,
+                      u'responsive_web': False,
+                      u'featured': True,
+                      u'last_modified': u'2015-07-31T19:21:07.562924Z',
+                      u'intended_audiences': [{u'audience': u'student'},
+                                              {u'audience': u'staff'},
+                                              {u'audience': u'faculty'},
+                                              {u'audience': u'freshman'}],
+                      u'resource_links':
+                          [{u'url': u'http://www.washington.edu/itconnect',
+                            u'link_type': u'IOS'}],
+                      u'id': 1,
+                      u'campus_tacoma': False}]
+        resources = MDOT()._python_list_to_resources_model_list(fake_list)
+        self.assertTrue(resources[0].has_ios())
+
+    # Make sure that has_ios returns false if the resource doesn't have an iOS link
+    def test_if_has_ios_false(self):
+        fake_list = [{u'accessible': False,
+                      u'feature_desc': u'This is a test',
+                      u'title': u'ITConnect',
+                      u'image': u'http://localhost:8000/media/\
+                      uploads/screenshot_CprR5Dk.jpg',
+                      u'created_date': u'2015-07-31T19:18:43.771637Z',
+                      u'campus_seattle': True,
+                      u'campus_bothell': False,
+                      u'responsive_web': False,
+                      u'featured': True,
+                      u'last_modified': u'2015-07-31T19:21:07.562924Z',
+                      u'intended_audiences': [{u'audience': u'student'},
+                                              {u'audience': u'staff'},
+                                              {u'audience': u'faculty'},
+                                              {u'audience': u'freshman'}],
+                      u'resource_links':
+                          [{u'url': u'http://www.washington.edu/itconnect',
+                            u'link_type': u'WEB'}],
+                      u'id': 1,
+                      u'campus_tacoma': False}]
+        resources = MDOT()._python_list_to_resources_model_list(fake_list)
+        self.assertFalse(resources[0].has_ios())
+
+    # Make sure that has_and returns true if the resource has an android link
+    def test_if_has_and_true(self):
+        fake_list = [{u'accessible': False,
+                      u'feature_desc': u'This is a test',
+                      u'title': u'ITConnect',
+                      u'image': u'http://localhost:8000/media/\
+                      uploads/screenshot_CprR5Dk.jpg',
+                      u'created_date': u'2015-07-31T19:18:43.771637Z',
+                      u'campus_seattle': True,
+                      u'campus_bothell': False,
+                      u'responsive_web': False,
+                      u'featured': True,
+                      u'last_modified': u'2015-07-31T19:21:07.562924Z',
+                      u'intended_audiences': [{u'audience': u'student'},
+                                              {u'audience': u'staff'},
+                                              {u'audience': u'faculty'},
+                                              {u'audience': u'freshman'}],
+                      u'resource_links':
+                          [{u'url': u'http://www.washington.edu/itconnect',
+                            u'link_type': u'AND'}],
+                      u'id': 1,
+                      u'campus_tacoma': False}]
+        resources = MDOT()._python_list_to_resources_model_list(fake_list)
+        self.assertTrue(resources[0].has_and())
+
+    # Make sure that has_and returns false if the resource doesn't have an android link
+    def test_if_has_and_false(self):
+        fake_list = [{u'accessible': False,
+                      u'feature_desc': u'This is a test',
+                      u'title': u'ITConnect',
+                      u'image': u'http://localhost:8000/media/\
+                      uploads/screenshot_CprR5Dk.jpg',
+                      u'created_date': u'2015-07-31T19:18:43.771637Z',
+                      u'campus_seattle': True,
+                      u'campus_bothell': False,
+                      u'responsive_web': False,
+                      u'featured': True,
+                      u'last_modified': u'2015-07-31T19:21:07.562924Z',
+                      u'intended_audiences': [{u'audience': u'student'},
+                                              {u'audience': u'staff'},
+                                              {u'audience': u'faculty'},
+                                              {u'audience': u'freshman'}],
+                      u'resource_links':
+                          [{u'url': u'http://www.washington.edu/itconnect',
+                            u'link_type': u'WEB'}],
+                      u'id': 1,
+                      u'campus_tacoma': False}]
+        resources = MDOT()._python_list_to_resources_model_list(fake_list)
+        self.assertFalse(resources[0].has_and())
+
+    # Make sure that has_wip returns true if the resource has a windows phone link
+    def test_if_has_wip_true(self):
+        fake_list = [{u'accessible': False,
+                      u'feature_desc': u'This is a test',
+                      u'title': u'ITConnect',
+                      u'image': u'http://localhost:8000/media/\
+                      uploads/screenshot_CprR5Dk.jpg',
+                      u'created_date': u'2015-07-31T19:18:43.771637Z',
+                      u'campus_seattle': True,
+                      u'campus_bothell': False,
+                      u'responsive_web': False,
+                      u'featured': True,
+                      u'last_modified': u'2015-07-31T19:21:07.562924Z',
+                      u'intended_audiences': [{u'audience': u'student'},
+                                              {u'audience': u'staff'},
+                                              {u'audience': u'faculty'},
+                                              {u'audience': u'freshman'}],
+                      u'resource_links':
+                          [{u'url': u'http://www.washington.edu/itconnect',
+                            u'link_type': u'WIP'}],
+                      u'id': 1,
+                      u'campus_tacoma': False}]
+        resources = MDOT()._python_list_to_resources_model_list(fake_list)
+        self.assertTrue(resources[0].has_wip())
+
+    # Make sure that has_wip returns false if the resource doesn't have a windows phone link
+    def test_if_has_wip_false(self):
+        fake_list = [{u'accessible': False,
+                      u'feature_desc': u'This is a test',
+                      u'title': u'ITConnect',
+                      u'image': u'http://localhost:8000/media/\
+                      uploads/screenshot_CprR5Dk.jpg',
+                      u'created_date': u'2015-07-31T19:18:43.771637Z',
+                      u'campus_seattle': True,
+                      u'campus_bothell': False,
+                      u'responsive_web': False,
+                      u'featured': True,
+                      u'last_modified': u'2015-07-31T19:21:07.562924Z',
+                      u'intended_audiences': [{u'audience': u'student'},
+                                              {u'audience': u'staff'},
+                                              {u'audience': u'faculty'},
+                                              {u'audience': u'freshman'}],
+                      u'resource_links':
+                          [{u'url': u'http://www.washington.edu/itconnect',
+                            u'link_type': u'WEB'}],
+                      u'id': 1,
+                      u'campus_tacoma': False}]
+        resources = MDOT()._python_list_to_resources_model_list(fake_list)
+        self.assertFalse(resources[0].has_wip())
+
     def tearDown(self):
         pass
