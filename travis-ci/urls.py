9a63548930d2d013e8965bd3bc3b2edc51d318dd
Craig M. Stimmel <cstimmel@uw.edu>
2015-08-05 13:51:04 -0700

    Synchronize travis-ci project with current settings and urls.

--- travis-ci/urls.py
+++ travis-ci/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -22,3 +24,6 @@ urlpatterns = [
     # url(r'^developers/', include('mdotdevs.urls')),
     url(r'^', include('mdot.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
