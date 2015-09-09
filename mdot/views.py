0b777f98ee6f86c22e848b45bb4a9bc0a86d13a9
Charlon Palacay <charlon@gmail.com>
2015-09-09 15:56:11 -0700

    Impliments htmlmin lib and fixes things that break minification.

--- mdot/views.py
+++ mdot/views.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from htmlmin.decorators import minified_response
 from mdot_rest_client.client import MDOT
 import urllib
 import json
 
 
+@minified_response
 def home(request):
     params = {'resources': MDOT().get_resources(featured=True)}
     return render_to_response('mdot/home.html', params,
