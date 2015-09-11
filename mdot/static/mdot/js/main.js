c04b530f3ccba6431ce0ea95543fcecbb0e035c9
Charlon Palacay <charlon@gmail.com>
2015-09-11 11:58:03 -0700

    Removes loadcss in favor of compressor and jquery method.

--- mdot/static/mdot/js/main.js
+++ mdot/static/mdot/js/main.js
@@ -3,6 +3,9 @@ $(function() {
     
     console.log("ready!");
     
+    // async load css by flipping the media attribute to all
+    $('link[rel="stylesheet"]').attr('media', 'all');
+    
     // truncate feature descriptions
     $('.mdot-resource-desc').succinct({
         size: 65
