496c6c09c59739276ad28c3c471654d9bc878803
haughar <haughar@uw.edu>
2015-10-09 11:51:31 -0700

    Get and display the student number if the user is a student.

--- mdot/test/person.py
+++ mdot/test/person.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.test import Client, TestCase
-from mdot.dao.pws import get_person_by_netid
+from mdot.dao import pws
 
 
 class MDOTPersonTest(TestCase):
@@ -34,7 +34,7 @@ class MDOTPersonTest(TestCase):
                               response.content)
 
             # assert that the DAO can return affiliation information
-            person = get_person_by_netid('javerage')
+            person = pws.get_person_by_netid('javerage')
 
             # expected_affiliations =
             # ["member","student","alum","staff","employee"]
@@ -45,6 +45,34 @@ class MDOTPersonTest(TestCase):
             self.assertTrue(person.is_staff)
             self.assertTrue(person.is_employee)
             self.assertEqual(None, person.is_faculty)
+
+    def test_get_student_number_by_username(self):
+        with self.settings(
+            RESTCLIENTS_PWS_DAO_CLASS='restclients.dao_implementation.pws.File'
+        ):
+            # create a user
+            javerage = User.objects.create_user('javerage',
+                                                'javerage@uw.edu',
+                                                'mytestpass')
+
+            # log the user in
+            client = Client()
+            response = client.post('/login/',
+                                   {'username': 'javerage',
+                                    'password': 'mytestpass',
+                                    'next': '/'},
+                                   follow=True)
+
+            self.assertEqual(200, response.status_code)
+            self.assertInHTML(('<div aria-hidden="true" class="mdot-netid">'
+                               '<span id="mdot_user_button">javerage</span>'
+                               '</div>'),
+                              response.content)
+            # call get_student_number_by_username
+            student_number = pws.get_student_number_by_netid('javerage')
+            # assert that student number = 1033334
+            self.assertEqual(student_number, "1033334")
+            pass
 
     def tearDown(self):
         pass
