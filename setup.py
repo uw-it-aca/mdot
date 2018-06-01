48a9701fc57679a3526f55e516710b7b787d479f
Craig M. Stimmel <cstimmel@uw.edu>
2018-06-01 12:41:57 -0700

    Revert "Try less than 2.0."
    This reverts commit 1bffae34a767e56943bb83027719fbe6dffcdc3b.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         'django<1.9rc1',
         'django-compressor',
         'django_mobileesp',
-        'uw-restclients<2.0',
+        'uw-restclients==1.1',
         'django-htmlmin',
     ],
     license='Apache License, Version 2.0',
