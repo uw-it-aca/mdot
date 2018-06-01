cd93e130ada22ae66c406ad67101e3b1bf892053
Craig M. Stimmel <cstimmel@uw.edu>
2018-06-01 12:33:18 -0700

    Pin to same version as on production.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         'django<1.9rc1',
         'django-compressor',
         'django_mobileesp',
-        'uw-restclients<2.0',
+        'uw-restclients==1.1.dev7',
         'django-htmlmin',
     ],
     license='Apache License, Version 2.0',
