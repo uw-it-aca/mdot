b4cf2ba407859705c9b0c5faa20d28b743aefeb9
Craig M. Stimmel <cstimmel@uw.edu>
2018-06-01 12:42:09 -0700

    Revert "Pinning restclients to 1.1."
    This reverts commit 0d33ddd392d33c7eca57289554feedd25b851ca2.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         'django<1.9rc1',
         'django-compressor',
         'django_mobileesp',
-        'uw-restclients==1.1',
+        'uw-restclients',
         'django-htmlmin',
     ],
     license='Apache License, Version 2.0',
