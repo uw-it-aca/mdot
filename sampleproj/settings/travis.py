4437b3dbfa2c03ddec62f9dcd9a21410b1d607be
Craig M. Stimmel <cstimmel@uw.edu>
2018-06-13 15:06:15 -0700

    Trim down travis settings even more.

--- sampleproj/settings/travis.py
+++ sampleproj/settings/travis.py
@@ -8,30 +8,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 from __future__ import absolute_import
-import os
 from .base import *
-
-from django_mobileesp.detector import mobileesp_agent as agent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'travis-xxxxxxxxxxxxxxxx'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = []
-
-STATIC_URL = '/static/'
-
-STATICFILES_FINDERS = (
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-    "compressor.finders.CompressorFinder",
-)
-
-MEDIA_ROOT = ''
-MEDIA_URL = '/media/'
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-RESTCLIENTS_MDOT_DAO_CLASS = 'mdot.mdot_rest_client.client.MDOTFile'
