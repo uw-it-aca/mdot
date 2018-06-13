8fe1b6d9dd5cae430306ce43144d467654b7973b
Craig M. Stimmel <cstimmel@uw.edu>
2018-06-13 15:10:55 -0700

    Move compressor settings to just local settings.

--- sampleproj/settings/base.py
+++ sampleproj/settings/base.py
@@ -111,18 +111,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-COMPRESS_ROOT = ""
-COMPRESS_PRECOMPILERS = (('text/less', 'lesscpy {infile} {outfile}'),)
-COMPRESS_ENABLED = False  # True if you want to compress your development build
-COMPRESS_OFFLINE = False  # True if you want to compress your build offline
-COMPRESS_CSS_FILTERS = [
-    'compressor.filters.css_default.CssAbsoluteFilter',
-    'compressor.filters.cssmin.CSSMinFilter'
-]
-COMPRESS_JS_FILTERS = [
-    'compressor.filters.jsmin.JSMinFilter',
-]
-
 # mobileesp
 
 DETECT_USER_AGENTS = {
--- sampleproj/settings/local.py
+++ sampleproj/settings/local.py
@@ -32,5 +32,17 @@ MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+COMPRESS_ROOT = ""
+COMPRESS_PRECOMPILERS = (('text/less', 'lesscpy {infile} {outfile}'),)
+COMPRESS_ENABLED = False  # True if you want to compress your development build
+COMPRESS_OFFLINE = False  # True if you want to compress your build offline
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter'
+]
+COMPRESS_JS_FILTERS = [
+    'compressor.filters.jsmin.JSMinFilter',
+]
+
 # RESTCLIENTS_MDOT_DAO_CLASS = 'mdot.mdot_rest_client.client.MDOTLive'
 RESTCLIENTS_MDOT_HOST = 'http://localhost:8000/'
