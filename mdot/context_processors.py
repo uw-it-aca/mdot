4e2ce61366c45089a785f4e3328c61424f1c936c
Aditya Kumar <adikumar@coconut.aca.uw.edu>
2018-07-19 11:32:11 -0700

    fixed pep8
    forgot to install the pre-commit hook again

--- mdot/context_processors.py
+++ mdot/context_processors.py
@@ -24,6 +24,7 @@ def devtools_bar(request):
         'devtools_bar': devtools
     }
 
+
 def get_emails(request):
     help_email = getattr(settings, "MDOT_HELP_EMAIL", None)
     ux_email = getattr(settings, "MDOT_UX_EMAIL", None)
