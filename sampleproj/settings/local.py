d696e99c89d8a285e6c20842766a3f5b76695630
Craig M. Stimmel <cstimmel@uw.edu>
2018-08-31 15:33:05 -0700

    Add string values to local settings so tests will pass against them.

--- sampleproj/settings/local.py
+++ sampleproj/settings/local.py
@@ -48,6 +48,6 @@ DETECT_USER_AGENTS = {
 RESTCLIENTS_MDOT_HOST = 'http://localhost:8000/'
 
 #Emails
-MDOT_HELP_EMAIL = None  # String for help desk email address
-MDOT_UX_EMAIL = None  # String for UX team email address
-MDOT_FORM_EMAIL = None  # String to email app publishing requests
+MDOT_HELP_EMAIL = 'help@example.edu' # String for help desk email address
+MDOT_UX_EMAIL = 'ux@example.edu' # String for UX team email address
+MDOT_FORM_EMAIL = 'serviceteam@example.edu' # String to email app publishing requests
