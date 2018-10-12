01723bbe8aed6ac1f15771cec2f25a9c1259989d
Charlon Palacay <charlon@gmail.com>
2018-10-11 21:18:27 -0700

    Updates setup version number and mdot name.

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='mdot-modern',
-    version='0.1',
+    name='mdot',
+    version='0.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
