3d4ff1dd2ef7e8ba04d25e38372ac86700dffb3f
Craig M. Stimmel <cstimmel@uw.edu>
2018-12-14 13:14:40 -0800

    Use render() instead of render_to_response to include the context.

--- mdot/views.py
+++ mdot/views.py
@@ -14,21 +14,21 @@ from forms import ReviewForm
 
 def home(request):
     params = {'resources': MDOT().get_resources(featured=True)}
-    return render_to_response('mdot/home.html', params)
+    return render(request, 'mdot/home.html', params)
 
 
 def developers(request):
-    return render_to_response(
+    return render(request,
         'mdot/developers/home.html')
 
 
 def guidelines(request):
-    return render_to_response(
+    return render(request,
         'mdot/developers/guidelines.html')
 
 
 def process(request):
-    return render_to_response(
+    return render(request,
         'mdot/developers/process.html')
 
 
