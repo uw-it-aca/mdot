d4416a608370eb8be3beb848ce70f9fa310bb849
erinRochfort99 <erochfor@uw.edu>
2019-12-10 15:49:33 -0800

    create initial models

--- mdot/models.py
+++ mdot/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.forms import ModelForm
 
 # Create your models here.
+
+class Sponsor(models.Model):
+    name = models.CharField(max_length = 50)
+    netid = models.CharField(max_length = 8)
+    title = models.CharField(max_length = 50)
+    email = models.EmailField(max_length = 30)
+    department = models.CharField(max_length = 30)
+    unit = models.CharField(max_length = 30)
+
+
+class Manager(models.Model):
+    name = models.CharField(max_length = 50)
+    netid = models.CharField(max_length = 8)
+
+
+class App(models.Model):
+    # define plarforms
+    name = models.CharField(max_length = 50)
+    primary_language = models.CharField(max_length = 20)  # are we using abbreviations?
+    request_date = models.DateTimeField(auto_now_add = True)
+    requester = models.ForeignKey(User)
+    app_manager = models.ForeignKey(Manager)
+    app_sponsor = models.ForeignKey(Sponsor)
+    app_sponser_agreed_date = models.DateTimeField(auto_now = True)
+    app_sponser_agreed = models.BooleanField(default = False)
+
+
+class Agreement(models.Model):
+    app = models.ForeignKey(App)
+    agree_time = models.DateTimeField(auto_now_add = True)
+
+
+class SponsorForm(ModelForm):
+    class Meta:
+        model = Sponsor
+        fields = '__all__'
+        labels = {
+        }
+        help_texts = {
+
+        }
+        error_messages = {
+
+        }
+
+
+class ManagerForm(ModelForm):
+    class Meta:
+        model = Manager
+        fields = '__all__'
+        labels = {
+        }
+        help_texts = {
+
+        }
+        error_messages = {
+
+        }
+
+
+class AppForm(ModelForm):
+    class Meta:
+        model = App
+        fields = ['name', 'primary_language']
+        labels = {
+        }
+        help_texts = {
+
+        }
+        error_messages = {
+
+        }
