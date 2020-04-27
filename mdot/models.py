76344d83e2ab307e6cbce4a3ecdfd62045d98dbd
erinRochfort99 <erochfor@uw.edu>
2020-04-27 13:10:54 -0700

    removed redundant fields and cleaned up migrations

--- mdot/models.py
+++ mdot/models.py
@@ -1,87 +1,70 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.forms import ModelForm
+from django import forms
 
 # Create your models here.
 
 
+class Platform(models.Model):
+    name = models.CharField(max_length=15)
+    app_store = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.app_store
+
+
 class Sponsor(models.Model):
-    name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     netid = models.CharField(max_length=16)
     title = models.CharField(max_length=50)
-    email = models.EmailField(max_length=40)
     department = models.CharField(max_length=30)
     unit = models.CharField(max_length=30)
 
 
 class Manager(models.Model):
-    name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     netid = models.CharField(max_length=16)
-    email = models.EmailField(max_length=40)
 
 
 class App(models.Model):
     name = models.CharField(max_length=50)
-    primary_language = models.CharField(max_length=20)  # abbreviations?
-    ios_platform = models.BooleanField(default=False)
-    android_platform = models.BooleanField(default=False)
+    primary_language = models.CharField(max_length=20)
+    platform = models.ManyToManyField(Platform)
     app_manager = models.ForeignKey(Manager)
     app_sponsor = models.ForeignKey(Sponsor)
     # requestor = models.OneToOneField(User)    # problem saving this
     request_date = models.DateTimeField(auto_now_add=True)
-    app_sponsor_agreed = models.BooleanField(default=False)
 
 
 class Agreement(models.Model):
     app = models.ForeignKey(App)
     agree_time = models.DateTimeField(auto_now_add=True)
-    sponsor = models.ForeignKey(Sponsor)
 
 
-class SponsorForm(ModelForm):
+class SponsorForm(forms.ModelForm):
     class Meta:
         model = Sponsor
         fields = '__all__'
-        labels = {
-
-        }
-        help_texts = {
-
-        }
-        error_messages = {
-
-        }
 
 
-class ManagerForm(ModelForm):
+class ManagerForm(forms.ModelForm):
     class Meta:
         model = Manager
         fields = '__all__'
-        labels = {
-
-        }
-        help_texts = {
-
-        }
-        error_messages = {
-
-        }
 
 
-class AppForm(ModelForm):
+class AppForm(forms.ModelForm):
     class Meta:
         model = App
-        fields = ['name', 'primary_language', 'ios_platform',
-                  'android_platform']
+        fields = ['name', 'primary_language', 'platform']
         labels = {
-            'android_platform':
-                'Add to the University of Washington Google Play Store',
-            'ios_platform':
-                'Add to the University of Washington Apple Store',
+            'platform': "Add to the University of Washington"
         }
-        help_texts = {
-            'primary_language': '(ENG for English)',
-        }
-        error_messages = {
 
-        }
+    def __init__(self, *args, **kwargs):
+        super(AppForm, self).__init__(*args, **kwargs)
+        self.fields['platform'].widget = forms.CheckboxSelectMultiple()
+        self.fields['platform'].queryset = Platform.objects.all()
+    
