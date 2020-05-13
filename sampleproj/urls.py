c4b678a11ea7efbfaa5b01c2d8f88dc59dfd9930
erinRochfort99 <erochfor@uw.edu>
2020-05-13 16:52:41 -0700

    updated imports

--- sampleproj/urls.py
+++ sampleproj/urls.py
@@ -13,15 +13,15 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # url(r'^admin/', include(admin.site.urls)),
-    # url(r'^api/v1/', include('mdot_rest.urls')),
-    url(r'^', include('mdot.urls')),
+    # path('admin/', include(admin.site.urls)),
+    # path('api/v1/', include('mdot_rest.urls')),
+    path('', include('mdot.urls')),
 ]
 
 if settings.DEBUG:
