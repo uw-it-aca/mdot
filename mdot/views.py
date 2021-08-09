007af44c14c6b7993fabb3938a9a1a45d4bf0d04
David Strupinski <david.strupinski@gmail.com>
2021-08-09 14:08:28 -0700

    fixes MDOT-94 by updating the README for usability

--- mdot/views.py
+++ mdot/views.py
@@ -20,6 +20,7 @@ from .models import SponsorForm, ManagerForm, AppForm,\
 def home(request):
     params = {"resources": MDOT().get_resources(featured=True)}
     return render(request, "mdot/home.html", params)
+    # return render(request, "mdot/home.html")
 
 
 def developers(request):
